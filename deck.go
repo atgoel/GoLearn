bda062dbc7e1e1cbb201ca90837b231d86179dbb
atgoel <atulgoyal115@hotmail.com>
2023-09-02 18:21:54 +0000

    new files

--- deck.go
+++ deck.go
@@ -1,39 +1,62 @@
 package main
-import {
-	fmt
-	strings
-	
-}
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type deck []string
 
 func newDeck() deck {
-	cardSuit:={"Spade", "Club", "Hearts", "Diamonds"}
-	cardRank := {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
-	for i:=0; i<4; i++ {
-        for j:=0; j<13; j++ {
-            deck = append(deck, cardRank[j]+" of "+cardSuit[i])
-        }
-    }
-	return deck
+	cardSuit := []string{"Spade", "Club", "Hearts", "Diamonds"}
+	cardRank := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	cards := deck{}
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 13; j++ {
+			cards = append(cards, cardRank[j]+" of "+cardSuit[i])
+		}
+	}
+	return cards
 
 }
 
-func (d deck) print{
-	for i, c := range deck
-	{
-        fmt.Println(i, c)
-    }
+func (d deck) print() {
+	for i, c := range d {
+		fmt.Println(i, c)
+	}
 }
 
-func (d deck) toString string{
-	return strings.join(deck,",")
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
-func hand(d deck, int size) (deck, deck){
+func hand(d deck, size int) (deck, deck) {
 	return d[:size], d[size:]
 }
 
-func (d deck) shuffle(){
+func (d deck) shuffle() {
 
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i, _ := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
 
+	}
+
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func readFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(bs), ","))
 }
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	mydeck := newDeck()
+	fmt.Println("Initialized Deck")
+	mydeck.print()
+
+	mydeck.shuffle()
+
+	fmt.Println("First hand")
+	a, _ := hand(mydeck, 10)
+	a.print()
+
+	fmt.Println("Write To file")
+	mydeck.saveToFile("myFile")
 
 }
